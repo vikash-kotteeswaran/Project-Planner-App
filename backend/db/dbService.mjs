09b531c3883591e4b925a627778dfbaa395c8c18
Newbie-master-416 <vikkykotty@gmail.com>
2022-07-06 23:14:40 +0530

    backend files has been shipped

---
+++ backend/db/dbService.mjs
@@ -0,0 +1,76 @@
+import * as dotenv from 'dotenv';
+import * as mysql from 'mysql';
+dotenv.config({path:'db/.env'})
+let instance = null;
+
+console.log(process.env.DB_PORT);
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+// const connection = mysql.createConnection({
+//     host: 'remotemysql.com',
+//     user: 'rd55m7yS9u',
+//     database: 'rd55m7yS9u',
+//     password: 'PFYAMddWsi',
+//     port: 3306,
+// });
+
+connection.connect((err) => {
+    if(err){
+        console.log(err);
+    } else{
+        console.log('db '+connection.state);
+    }
+});
+
+class DbService {
+
+    static getInstance(){
+        return instance? instance : new DbService();
+    }
+
+    async getAllData(){ 
+        try{
+            const resp = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM user;";
+    
+                connection.query(query, (err, res) => {
+                    if(err) reject(new Error(err.message));
+                    else resolve(res);
+                });
+            })
+
+            return resp;
+        } catch(err){
+            console.log(err);
+        }
+    }
+
+    async addRow(name, password) {
+        try{
+            const resp = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO user (name, password, date) VALUES (?, ?, CURRENT_TIMESTAMP)";
+
+                connection.query(query, [name, password], (err, res) => {
+                    if(err) reject(new Error(err.message));
+                    else resolve(res);
+                })
+            })
+
+            console.log('server resp: ', resp)
+
+            return {'name': name, 'password': password};
+
+        } catch(err) {
+            console.log(err);
+        }
+    }
+}
+
+export default DbService;
