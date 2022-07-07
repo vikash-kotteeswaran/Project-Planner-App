7495c69d28eafc6b6fe4e2a11119e7ffaf8bc0a5
Newbie-master-416 <vikkykotty@gmail.com>
2022-07-08 00:45:31 +0530

    dbService.js and server.js has been edited to include features such as updating rows, deleting rows, searching rows through queries

--- backend/db/dbService.mjs
+++ backend/db/dbService.mjs
@@ -38,15 +38,18 @@ class DbService {
                     if(err) reject(new Error(err.message));
                     else resolve(res);
                 });
-            })
+            });
+
+            console.log('server resp: ', resp);
 
             return resp;
         } catch(err){
             console.log(err);
-        }
-    }
+        };
+    };
 
     async addRow(name, password) {
+        // res - result, resp - response, reject, resolve, err - error
         try{
             const resp = await new Promise((resolve, reject) => {
                 const query = "INSERT INTO user (name, password, date) VALUES (?, ?, CURRENT_TIMESTAMP)";
@@ -54,17 +57,91 @@ class DbService {
                 connection.query(query, [name, password], (err, res) => {
                     if(err) reject(new Error(err.message));
                     else resolve(res);
-                })
-            })
+                });
+            });
 
-            console.log('server resp: ', resp)
+            console.log('server resp: ', resp);
 
-            return {'name': name, 'password': password};
+            return {'affectedRows': resp.affectedRows, 'insertId': resp.insertId};
 
         } catch(err) {
             console.log(err);
+        };
+    };
+
+    async updateRow(id, colNvalDict){
+        try{
+            const resp = await new Promise((resolve, reject) => {
+                let query = "UPDATE user SET";
+                const entries = Object.entries(colNvalDict);
+                query += ` ${entries[0][0]} = '${entries[0][1]}' `;
+
+                for(let i of entries.slice(1,)){
+                    query += `, ${i[0]} = '${i[1]}' `;
+                }
+                
+                query += "WHERE id = ?;";
+
+                connection.query(query, [id], (err, res) => {
+                    if(err) reject(new Error(err.message));
+                    else resolve(res);
+                });
+            });
+
+            console.log('server resp: ', resp);
+
+            return {'changedRows': resp.changedRows, 'affectedRows': resp.affectedRows};
+            
+        } catch(err){
+            console.log(err);
+        };
+    };
+
+    async deleteRow(id){
+        try{
+            const resp = await new Promise((resolve, reject) => {
+                const query = "DELETE FROM user WHERE id = ?;"
+
+                connection.query(query, [id], (err, res) => {
+                    if(err) reject(new Error(err.message));
+                    else resolve(res);
+                }); 
+            });
+
+            console.log('server resp: ', resp);
+
+            return {'changedRows': resp.changedRows, 'affectedRows': resp.affectedRows};
+
+        } catch(err){
+            console.log(err);
+        };
+    };
+
+    async searchRows(type, oldQuery, nature, column, value){
+        try{
+            const resp = await new Promise((resolve, reject) => {
+                let query = oldQuery? oldQuery.replace(';', '') + ` ${nature} ` : `SELECT * FROM user WHERE `;
+
+                if(type == 'LIKE'){
+                    value = `%${value}%`;
+                }
+
+                query += `${column} ${type} '${value}';`;
+
+                connection.query(query, (err, res) => {
+                    if(err) reject(new Error(err.message));
+                    else resolve({'data' : res, 'query': query});
+                });
+            });
+
+            console.log('server resp: ', resp);
+
+            return resp;
+
+        } catch(err){
+            console.log(err);
         }
     }
-}
+};
 
 export default DbService;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+// FETCH api POST call to add new user to the table
+// fetch('/api/addUser', {
+//     'headers':{
+//         'content-type': 'application/json'
+//     },
+//     'method': 'POST',
+//     'body': JSON.stringify({'name': 'sidd', 'password': 'mentalistpj'})
+// })
+// .then(response => {
+//     console.log(response);
+//     return response.json();
+// })
+// .then(data => console.log(data));
+////////////////////////////////////////////////////////////////////////////////
+
 import DbService from "./db/dbService.mjs";
 import express from 'express';
 import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.get('/api', (req, res) => {
-    res.json({'fuck': 'you'});
+    res.json({'Welcome': 'Mutton kuska wa'});
 })
 
 app.get('/api/getAll', (req, res) => {
@@ -16,10 +32,7 @@ app.get('/api/getAll', (req, res) => {
     const result = DbInstance.getAllData();
 
     result
-    .then(data => {
-        console.log(data); 
-        res.json({'data': data});
-    })
+    .then(data => res.json({'success': {'data': data}}))
     .catch(err => console.log(err));
 })
 
@@ -29,28 +42,52 @@ app.post('/api/addUser', (req, res) => {
     const result = DbInstance.addRow(name, password);
 
     result
-    .then(data => {
-        console.log('server data: ', data);
-        res.json({'success': data});
-    })
+    .then(data => res.json({'success': data}))
     .catch(err => console.log(err));
+})
 
+app.put('/api/updateData', (req, res) => {
+    const id = req.body.id;
+    const fieldsDict = req.body.fieldsValues;
+    const DbInstance = DbService.getInstance();
+    const result = DbInstance.updateRow(id, fieldsDict);
 
-    ///////////////////////////////////////////////////////////////////////////////
-    // FETCH api POST call to add new user to the table
-    // fetch('/api/addUser', {
-    //     'headers':{
-    //         'content-type': 'application/json'
-    //     },
-    //     'method': 'POST',
-    //     'body': JSON.stringify({'name': 'sidd', 'password': 'mentalistpj'})
-    // })
-    // .then(response => {
-    //     console.log(response);
-    //     return response.json();
-    // })
-    // .then(data => console.log(data));
-    ////////////////////////////////////////////////////////////////////////////////
+    result
+    .then(data => res.json({'success': data}))
+    .catch(err => console.log(err))
+})
+
+app.delete('/api/deleteData', (req, res) => {
+    const id = req.body.id;
+
+    const DbInstance = DbService.getInstance();
+    const result =  DbInstance.deleteRow(id);
+
+    result
+    .then(data => res.json({'success': data}))
+    .catch(err => console.log(err));
+})
+
+app.post('/api/searchData', (req, res) => {
+    const type = req.body.type.toUpperCase();
+    if(type != 'LIKE' && type != '>' && type != '<' && type != '>=' && type != '<=' && type != '='){
+        throw new Error('type of search is invalid');
+    }
+
+    const nature = req.body.nature.toUpperCase();
+    if(nature != 'AND' && nature != 'OR'){
+        throw new Error('nature of search is invalid');
+    }
+
+    const [field, value] = Object.entries(req.body.searchElement)[0];
+    const oldQuery = req.body.oldQuery;
+
+    const DbInstance = DbService.getInstance();
+    const result =  DbInstance.searchRows(type, oldQuery, nature, field, value);
+
+    result
+    .then(data => res.json({'success': data}))
+    .catch(err => console.log(err));
 })
 
 app.listen(process.env.PORT || 3030, () => console.log('app is listening'));
