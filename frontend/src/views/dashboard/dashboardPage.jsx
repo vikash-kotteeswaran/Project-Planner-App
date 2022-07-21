53414b71f8fc41715b128b4a6efac8f4e0612993
Newbie-master-416 <vikkykotty@gmail.com>
2022-07-21 23:21:18 +0530

    dashboard sidebar pull transition has been created and enhanced - myProjects view has been initiated with project cards

--- frontend/src/views/dashboard/dashboardPage.jsx
+++ frontend/src/views/dashboard/dashboardPage.jsx
@@ -39,33 +39,38 @@ const DashboardPage = () => {
     return (
         <div className='dashboard'>
             <div className='sidebar'>
+                {/* select view divs */}
+                <div className='sidebar-app-logo' id='app-logo'>
+                    <span className='sidebar-ops-icon'>PP</span>
+                    <span className='sidebar-ops'>Project Planner</span>
+                </div>
                 <div className='sidebar-view-ops' id='my-projects' onClick={() => {setView('my-projects')}}>
-                    <span className='sidebar-ops'>My Projects</span>
                     <span className='sidebar-ops-icon'>M</span>
+                    <span className='sidebar-ops'>My Projects</span>
                 </div>
                 <div className='sidebar-view-ops' id='projects' onClick={() => {setView('projects')}}>
-                    <span className='sidebar-ops'>Projects</span>
                     <span className='sidebar-ops-icon'>P</span>
+                    <span className='sidebar-ops'>Projects</span>
                 </div>
                 <div className='sidebar-view-ops' id='tasks' onClick={() => {setView('tasks')}}>
-                    <span className='sidebar-ops'>Tasks</span>
                     <span className='sidebar-ops-icon'>T</span>
+                    <span className='sidebar-ops'>Tasks</span>
                 </div>
                 <div className='sidebar-view-ops' id='stats' onClick={() => {setView('stats')}}>
-                    <span className='sidebar-ops'>Stats</span>
                     <span className='sidebar-ops-icon'>S</span>
+                    <span className='sidebar-ops'>Stats</span>
                 </div>
                 <div className='sidebar-view-ops' id='profile' onClick={() => {setView('profile')}}>
-                    <span className='sidebar-ops'>Profile</span>
                     <span className='sidebar-ops-icon'>P</span>
+                    <span className='sidebar-ops'>Profile</span>
                 </div>
                 <div className='sidebar-view-ops' id='settings' onClick={() => {setView('settings')}}>
-                    <span className='sidebar-ops'>Settings</span>
                     <span className='sidebar-ops-icon'>S</span>
+                    <span className='sidebar-ops'>Settings</span>
                 </div>
                 <div className='sidebar-view-ops' id='logout' onClick={onButton}>
-                    <span className='sidebar-ops'>Logout</span>
                     <span className='sidebar-ops-icon'>L</span>
+                    <span className='sidebar-ops'>Logout</span>
                 </div>
             </div>
             <div className='main-view'>
--- frontend/src/views/dashboard/myProjects/myProjects.jsx
+++ frontend/src/views/dashboard/myProjects/myProjects.jsx
@@ -3,9 +3,42 @@ import './myProjects.css';
 
 const MyProjects = () => {
 
+    const projects = [
+        {
+            title: 'To Do App',
+            admin: 'vikashk',
+            desc: 'make a To Do App with react and redux',
+            members: 'jinjoe, sidd, aniroot, rajku'
+        },
+        {
+            title: 'Zoom Clone App',
+            admin: 'vikashk',
+            desc: 'make a Zoom Clone App with react and redux',
+            members: 'jinjoe, sidd, aniroot, rajku'
+        }
+    ]
+
+    const projectDivs = () => {
+        let mainDiv = [];
+        
+        for (let project of projects){
+            console.log(project, project.admin);
+            mainDiv.push(
+                <div className='project-card'>
+                    <div className='project-title'>{project.title}</div>
+                    <div className='project-admin'>{project.admin}</div>
+                    <div className='project-desc'>{project.desc}</div>
+                </div>
+            );
+        };
+
+        return (<div className='projects-main-div'>{mainDiv}</div>);
+    };
+
     return(
         <div className='my-projects-view'>
             <h1>My Projects</h1>
+            {projectDivs()}
         </div>
     );
 };
